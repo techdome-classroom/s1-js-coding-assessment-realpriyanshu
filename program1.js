3fac3c5e4ea42361476901ee4e327602f669483b
Priyanshu Mewada <oppriyanshu@gmail.com>
2024-09-30 12:19:14 +0530

    assesment done (Priyanshu mewada)

--- program1.js
+++ program1.js
@@ -3,6 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    var stack = [];
+    var pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (var i = 0; i < s.length; i++) {
+        var char = s[i];
+        if (pairs[char]) {
+            stack.push(char);
+        } else {
+            var lastOpen = stack.pop();
+            if (pairs[lastOpen] !== char) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
     
 };
 
--- program2.js
+++ program2.js
@@ -3,6 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+
+    var roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    var total = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        var current = roman[s[i]];
+        var next = roman[s[i + 1]];
+
+        if (next > current) {
+            total += next - current;
+            i++;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
     
 };
 
